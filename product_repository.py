3528aa26909dfc618d6c38e7841ac01dc34a2a5e
Anna S <annierose1213@gmail.com>
2024-07-30 22:06:13 +0200

    Refactor: change the if statement to the match statement

--- product_repository.py
+++ product_repository.py
@@ -1,4 +1,3 @@
-# TODO switchcase python 3.10 instead if-else
 # TODO improve naming
 # TODO add repr/str to classes
 # TODO create database (postgreSQL) to store products
@@ -18,25 +17,26 @@ def add_products(json_file):
         all_products = json.loads(all_products)
 
         for product in all_products:
-            if product["product"] == "fruit":
-                name = product["name"]
-                freshness_in_days = product["freshness_in_days"]
-                product_list.append(products.Fruit(name, freshness_in_days))
-            elif product["product"] == "vegetable":
-                name = product["name"]
-                freshness_in_days = product["freshness_in_days"]
-                product_list.append(products.Vegetable(name, freshness_in_days))
-            elif product["product"] == "dairy":
-                name = product["name"]
-                expiry_date = product["expiry_date"]
-                product_list.append(products.Dairy(name, expiry_date))
-            elif product["product"] == "meat":
-                name = product["name"]
-                expiry_date = product["expiry_date"]
-                product_list.append(products.Meat(name, expiry_date))
-            elif product["product"] == "grain":
-                name = product["name"]
-                expiry_date = product["expiry_date"]
-                product_list.append(products.Grain(name, expiry_date))
+            match product["product"]:
+                case "fruit":
+                    name = product["name"]
+                    freshness_in_days = product["freshness_in_days"]
+                    product_list.append(products.Fruit(name, freshness_in_days))
+                case "vegetable":
+                    name = product["name"]
+                    freshness_in_days = product["freshness_in_days"]
+                    product_list.append(products.Vegetable(name, freshness_in_days))
+                case "dairy":
+                    name = product["name"]
+                    expiry_date = product["expiry_date"]
+                    product_list.append(products.Dairy(name, expiry_date))
+                case "meat":
+                    name = product["name"]
+                    expiry_date = product["expiry_date"]
+                    product_list.append(products.Meat(name, expiry_date))
+                case "grain":
+                    name = product["name"]
+                    expiry_date = product["expiry_date"]
+                    product_list.append(products.Grain(name, expiry_date))
 
     print(product_list)
