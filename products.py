a75ad39dad30a37668fed0c9f4562ad855624319
Anna S <annierose1213@gmail.com>
2024-07-31 17:02:05 +0200

    Refactor: improve the code structure of the classes

--- products.py
+++ products.py
@@ -2,36 +2,28 @@ from datetime import date, timedelta
 
 
 class Product:
+    expiration = "expiry date not set"
+
     def __init__(self, name):
         self.name = name
+        self.expiry_date = Product.expiration
+
+    def __repr__(self):
+        return f"({self.name}, expiry date: {self.expiry_date})"
 
 
 class Fruit(Product):
     def __init__(self, name, freshness_in_days):
         super().__init__(name)
         self.freshness_in_days = freshness_in_days
-        self.expiry_date = None
-
-    def __repr__(self):
-        return f"({self.name}, {self.freshness_in_days})"
-
-    def calculate_date(self):
         self.expiry_date = date.today() + timedelta(days=self.freshness_in_days)
-        return self.expiry_date
 
 
 class Vegetable(Product):
     def __init__(self, name, freshness_in_days):
         super().__init__(name)
         self.freshness_in_days = freshness_in_days
-        self.expiry_date = None
-
-    def __repr__(self):
-        return f"({self.name}, {self.freshness_in_days})"
-
-    def calculate_date(self):
         self.expiry_date = date.today() + timedelta(days=self.freshness_in_days)
-        return self.expiry_date
 
 
 class Dairy(Product):
@@ -39,28 +31,14 @@ class Dairy(Product):
         super().__init__(name)
         self.expiry_date = expiry_date
 
-    def __repr__(self):
-        return f"({self.name}, {self.expiry_date})"
-
 
 class Meat(Product):
     def __init__(self, name, expiry_date):
         super().__init__(name)
         self.expiry_date = expiry_date
 
-    def __repr__(self):
-        return f"({self.name}, {self.expiry_date})"
-
 
 class Grain(Product):
     def __init__(self, name, expiry_date):
         super().__init__(name)
         self.expiry_date = expiry_date
-
-    def __repr__(self):
-        return f"({self.name}, {self.expiry_date})"
-
-
-# fruit1 = Fruit("mango", 5)
-#
-# print(fruit1.calculate_date())
