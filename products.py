873fec07f825e1b866210834c5fec65821181852
Anna S <annierose1213@gmail.com>
2024-07-31 18:52:34 +0200

    Refactor: Create calculate_date function

--- products.py
+++ products.py
@@ -12,18 +12,23 @@ class Product:
         return f"({self.name}, expiry date: {self.expiry_date})"
 
 
+def calculate_date(freshness_in_days):
+    expiry_date = date.today() + timedelta(days=freshness_in_days)
+    return expiry_date
+
+
 class Fruit(Product):
     def __init__(self, name, freshness_in_days):
         super().__init__(name)
         self.freshness_in_days = freshness_in_days
-        self.expiry_date = date.today() + timedelta(days=self.freshness_in_days)
+        self.expiry_date = calculate_date(self.freshness_in_days)
 
 
 class Vegetable(Product):
     def __init__(self, name, freshness_in_days):
         super().__init__(name)
         self.freshness_in_days = freshness_in_days
-        self.expiry_date = date.today() + timedelta(days=self.freshness_in_days)
+        self.expiry_date = calculate_date(self.freshness_in_days)
 
 
 class Dairy(Product):
