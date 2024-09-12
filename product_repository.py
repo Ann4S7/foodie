a837d48a2e6344dacbde5d69e65f7f3ae2abb6fe
Anna S <annierose1213@gmail.com>
2024-09-12 16:01:42 +0200

    Refactor: Get quantity value from json file.

--- product_repository.py
+++ product_repository.py
@@ -17,18 +17,19 @@ def add_products(json_file):
         for product in all_products:
             match product["product"]:
                 case "fruit":
-                    new_product = products.Fruit(product["name"], product["freshness_in_days"])
+                    new_product = products.Fruit(product["name"], product["freshness_in_days"], product.get("quantity"))
 
                 case "vegetable":
-                    new_product = products.Vegetable(product["name"], product["freshness_in_days"])
+                    new_product = products.Vegetable(product["name"], product["freshness_in_days"],
+                                                     product.get("quantity"))
 
                 case "dairy":
-                    new_product = products.Dairy(product["name"], product["expiry_date"])
+                    new_product = products.Dairy(product["name"], product["expiry_date"], product.get("quantity"))
 
                 case "meat":
-                    new_product = products.Meat(product["name"], product["expiry_date"])
+                    new_product = products.Meat(product["name"], product["expiry_date"], product.get("quantity"))
 
                 case "grain":
-                    new_product = products.Grain(product["name"], product["expiry_date"])
+                    new_product = products.Grain(product["name"], product["expiry_date"], product.get("quantity"))
 
             new_product.save()
--- products.py
+++ products.py
@@ -27,40 +27,45 @@ class Product:
 class Fruit(Product):
     CATEGORY = "fruit"
 
-    def __init__(self, name, freshness_in_days):
+    def __init__(self, name, freshness_in_days, quantity):
         super().__init__(name)
         self.freshness_in_days = freshness_in_days
         self.expiry_date = calculate_date(self.freshness_in_days)
+        self.quantity = quantity or self.DEFAULT_QUANTITY
 
 
 class Vegetable(Product):
     CATEGORY = "vegetable"
 
-    def __init__(self, name, freshness_in_days):
+    def __init__(self, name, freshness_in_days, quantity):
         super().__init__(name)
         self.freshness_in_days = freshness_in_days
         self.expiry_date = calculate_date(self.freshness_in_days)
+        self.quantity = quantity or self.DEFAULT_QUANTITY
 
 
 class Dairy(Product):
     CATEGORY = "diary"
 
-    def __init__(self, name, expiry_date):
+    def __init__(self, name, expiry_date, quantity):
         super().__init__(name)
         self.expiry_date = expiry_date
+        self.quantity = quantity or self.DEFAULT_QUANTITY
 
 
 class Meat(Product):
     CATEGORY = "meat"
 
-    def __init__(self, name, expiry_date):
+    def __init__(self, name, expiry_date, quantity):
         super().__init__(name)
         self.expiry_date = expiry_date
+        self.quantity = quantity or self.DEFAULT_QUANTITY
 
 
 class Grain(Product):
     CATEGORY = "grain"
 
-    def __init__(self, name, expiry_date):
+    def __init__(self, name, expiry_date, quantity):
         super().__init__(name)
         self.expiry_date = expiry_date
+        self.quantity = quantity or self.DEFAULT_QUANTITY
