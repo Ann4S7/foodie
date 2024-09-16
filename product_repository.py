d06e0c9c8fa9eb2d3a529d88824c57f0305a7828
Ann4S7 <151684781+Ann4S7@users.noreply.github.com>
2024-09-16 12:58:59 +0200

    Number of products (#4)
    * Chore: Add class attribute QUANTITY.
    
    * Refactor: Get quantity value from json file.
    
    * Refactor: Add the ability to update product quantity.
    
    * Chore: Update json test file.
    
    * Refactor: Change code structure.
    
    * Refactor: Improve variable names and code structure.

--- product_repository.py
+++ product_repository.py
@@ -6,29 +6,88 @@
 
 import json
 
+from database_context_manager import DatabaseContextManager
 import products
+
+
+class Repository:
+
+    def get(self, *args, **kwargs):
+        """Get the row from database."""
+        raise NotImplemented
+
+    def add(self, *args, **kwargs):
+        """Add the row to database."""
+        raise NotImplemented
+
+    def update(self, *args, **kwargs):
+        """Update the row in database."""
+        raise NotImplemented
+
+    def remove(self, *args, **kwargs):
+        """Remove the row from database."""
+        raise NotImplemented
+
+
+class ProductRepository(Repository):
+
+    def get(self, product):
+        with DatabaseContextManager(database="foodie_db", user="postgres", password="new_password",
+                                    host="localhost", port=5432) as cursor:
+
+            cursor.execute(f"SELECT * FROM products "
+                           f"WHERE name = '{product.name}' AND expiry_date = '{product.expiry_date}';")
+
+            return cursor.fetchall()
+
+    def add(self, product):
+        with DatabaseContextManager(database="foodie_db", user="postgres", password="new_password",
+                                    host="localhost", port=5432) as cursor:
+
+            cursor.execute(f"INSERT INTO products(category, name, expiry_date, quantity)"
+                           f"VALUES('{product.CATEGORY}', '{product.name}',"
+                           f"'{product.expiry_date}', {product.quantity});")
+
+    def update(self, product):
+        with DatabaseContextManager(database="foodie_db", user="postgres", password="new_password",
+                                    host="localhost", port=5432) as cursor:
+
+            cursor.execute(f"UPDATE products SET quantity = quantity + {product.quantity} "
+                           f"WHERE name = '{product.name}' AND expiry_date = '{product.expiry_date}';")
 
 
 def add_products(json_file):
     with open(json_file) as file:
-        all_products = file.read()
-        all_products = json.loads(all_products)
+        products_list = file.read()
+        products_list = json.loads(products_list)
 
-        for product in all_products:
-            match product["product"]:
+        for product_dict in products_list:
+            match product_dict["product"]:
                 case "fruit":
-                    new_product = products.Fruit(product["name"], product["freshness_in_days"])
+                    product = products.Fruit(product_dict["name"], product_dict["freshness_in_days"],
+                                             product_dict.get("quantity"))
 
                 case "vegetable":
-                    new_product = products.Vegetable(product["name"], product["freshness_in_days"])
+                    product = products.Vegetable(product_dict["name"], product_dict["freshness_in_days"],
+                                                 product_dict.get("quantity"))
 
                 case "dairy":
-                    new_product = products.Dairy(product["name"], product["expiry_date"])
+                    product = products.Dairy(product_dict["name"], product_dict["expiry_date"],
+                                             product_dict.get("quantity"))
 
                 case "meat":
-                    new_product = products.Meat(product["name"], product["expiry_date"])
+                    product = products.Meat(product_dict["name"], product_dict["expiry_date"],
+                                            product_dict.get("quantity"))
 
                 case "grain":
-                    new_product = products.Grain(product["name"], product["expiry_date"])
+                    product = products.Grain(product_dict["name"], product_dict["expiry_date"],
+                                             product_dict.get("quantity"))
 
-            new_product.save()
+            repo = ProductRepository()
+
+            product_status = repo.get(product)
+
+            if product_status:
+                repo.update(product)
+            else:
+                repo.add(product)
--- products.py
+++ products.py
@@ -1,64 +1,62 @@
 from utils import calculate_date
 
-from database_context_manager import DatabaseContextManager
-
 
 class Product:
     EXPIRY_DATE_NOT_SET = "expiry date not set"
     CATEGORY = "product"
+    DEFAULT_QUANTITY = 1
 
     def __init__(self, name):
         self.name = name
         self.expiry_date = Product.EXPIRY_DATE_NOT_SET
+        self.quantity = Product.DEFAULT_QUANTITY
 
     def __repr__(self):
         return f"({self.name}, expiry date: {self.expiry_date})"
-
-    def save(self):
-        with DatabaseContextManager(database="foodie_db", user="postgres", password="new_password",
-                                    host="localhost", port=5432) as cursor:
-            cursor.execute(f"INSERT INTO products(category, name, expiry_date)"
-                           f"VALUES('{self.CATEGORY}', '{self.name}',"
-                           f"'{self.expiry_date}');")
 
 
 class Fruit(Product):
     CATEGORY = "fruit"
 
-    def __init__(self, name, freshness_in_days):
+    def __init__(self, name, freshness_in_days, quantity):
         super().__init__(name)
         self.freshness_in_days = freshness_in_days
         self.expiry_date = calculate_date(self.freshness_in_days)
+        self.quantity = quantity or self.DEFAULT_QUANTITY
 
 
 class Vegetable(Product):
     CATEGORY = "vegetable"
 
-    def __init__(self, name, freshness_in_days):
+    def __init__(self, name, freshness_in_days, quantity):
         super().__init__(name)
         self.freshness_in_days = freshness_in_days
         self.expiry_date = calculate_date(self.freshness_in_days)
+        self.quantity = quantity or self.DEFAULT_QUANTITY
 
 
 class Dairy(Product):
     CATEGORY = "diary"
 
-    def __init__(self, name, expiry_date):
+    def __init__(self, name, expiry_date, quantity):
         super().__init__(name)
         self.expiry_date = expiry_date
+        self.quantity = quantity or self.DEFAULT_QUANTITY
 
 
 class Meat(Product):
     CATEGORY = "meat"
 
-    def __init__(self, name, expiry_date):
+    def __init__(self, name, expiry_date, quantity):
         super().__init__(name)
         self.expiry_date = expiry_date
+        self.quantity = quantity or self.DEFAULT_QUANTITY
 
 
 class Grain(Product):
     CATEGORY = "grain"
 
-    def __init__(self, name, expiry_date):
+    def __init__(self, name, expiry_date, quantity):
         super().__init__(name)
         self.expiry_date = expiry_date
+        self.quantity = quantity or self.DEFAULT_QUANTITY
