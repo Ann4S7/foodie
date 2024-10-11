5523f7a4ba303d83eca33a5d12ecd5d2f5ae1ca5
Anna S <annierose1213@gmail.com>
2024-10-11 21:42:11 +0200

    Feat: Improve the search method and create the display_products function.

--- cli.py
+++ cli.py
@@ -21,6 +21,10 @@ remove_parser = subparsers.add_parser("remove")
 remove_parser.add_argument("json_file_remove")
 remove_parser.set_defaults(func=product_repository.remove_products)
 
+display_parser = subparsers.add_parser("display")
+display_parser.add_argument("json_file_display")
+display_parser.set_defaults(func=product_repository.display_products)
+
 args = parser.parse_args()
 
 args.func(args)
