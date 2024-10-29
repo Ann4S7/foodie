2d7adbf592e6c074c30a2b43d6a831e79f84a696
Ann4S7 <151684781+Ann4S7@users.noreply.github.com>
2024-10-29 13:27:18 +0100

    Chore: Code formatting (#12)
    * Chore: Move conflog.py to src folder.
    * Chore: Code formatting.
    * Chore: Sort imports.
    * Chore: Pylint configuration.
    * Refactor: Improve the code.
    * Chore: Pre-commit configuration.
    * Chore: Create dev_requirements.txt.

--- src/db_utils.py
+++ src/db_utils.py
@@ -2,19 +2,23 @@ from database_context_manager import DatabaseContextManager
 
 
 def create_table(database, user, password, host, port):
-    with DatabaseContextManager(database=database, user=user, password=password,
-                                host=host, port=port) as cursor:
+    with DatabaseContextManager(
+        database=database, user=user, password=password, host=host, port=port
+    ) as cursor:
 
-        cursor.execute(f"CREATE TABLE products("
-                       f"product_id SERIAL PRIMARY KEY, "
-                       f"category VARCHAR(50) NOT NULL, "
-                       f"name VARCHAR(50) NOT NULL, "
-                       f"expiry_date date NOT NULL, "
-                       f"quantity INTEGER);")
+        cursor.execute(
+            "CREATE TABLE products("
+            "product_id SERIAL PRIMARY KEY, "
+            "category VARCHAR(50) NOT NULL, "
+            "name VARCHAR(50) NOT NULL, "
+            "expiry_date date NOT NULL, "
+            "quantity INTEGER);"
+        )
 
 
 def drop_table(database, user, password, host, port):
-    with DatabaseContextManager(database=database, user=user, password=password,
-                                host=host, port=port) as cursor:
+    with DatabaseContextManager(
+        database=database, user=user, password=password, host=host, port=port
+    ) as cursor:
 
-        cursor.execute(f"DROP TABLE products;")
+        cursor.execute("DROP TABLE products;")
--- tests/integration_tests/test_product_repository.py
+++ tests/integration_tests/test_product_repository.py
@@ -1,8 +1,14 @@
-from argparse import Namespace
-import pytest
 import datetime
+from argparse import Namespace
 
-from product_repository import add_products, display_products, ProductRepository, remove_products
+import pytest
+
+from product_repository import (
+    ProductRepository,
+    add_products,
+    display_products,
+    remove_products,
+)
 from utils import calculate_date
 
 
@@ -11,6 +17,7 @@ def product_repository():
     return ProductRepository()
 
 
+# pylint: disable=redefined-outer-name, unused-argument
 @pytest.mark.order(1)
 def test_add_products(product_repository, init_resource):
     # given
@@ -22,35 +29,47 @@ def test_add_products(product_repository, init_resource):
 
     # then
     assert product_repository.count() == 2
-    assert (product_repository.search(conditions={"name": "ham", "expiry_date": datetime.date(2024, 9, 30)})
-            == [(1, "meat", "ham", datetime.date(2024, 9, 30), 1)])
-    assert (product_repository.search(conditions={"name": "banana",  "expiry_date": calculate_date(3)})
-            == [(2, "fruit", "banana",  calculate_date(3), 2)])
+    assert product_repository.search(
+        conditions={"name": "ham", "expiry_date": datetime.date(2024, 9, 30)}
+    ) == [(1, "meat", "ham", datetime.date(2024, 9, 30), 1)]
+    assert product_repository.search(
+        conditions={"name": "banana", "expiry_date": calculate_date(3)}
+    ) == [(2, "fruit", "banana", calculate_date(3), 2)]
 
     # when
     add_products(namespace)
 
     # then
     assert product_repository.count() == 2
-    assert (product_repository.search(conditions={"name": "ham", "expiry_date": datetime.date(2024, 9, 30)})
-            == [(1, "meat", "ham", datetime.date(2024, 9, 30), 2)])
-    assert (product_repository.search(conditions={"name": "banana",  "expiry_date": calculate_date(3)})
-            == [(2, "fruit", "banana",  calculate_date(3), 4)])
+    assert product_repository.search(
+        conditions={"name": "ham", "expiry_date": datetime.date(2024, 9, 30)}
+    ) == [(1, "meat", "ham", datetime.date(2024, 9, 30), 2)]
+    assert product_repository.search(
+        conditions={"name": "banana", "expiry_date": calculate_date(3)}
+    ) == [(2, "fruit", "banana", calculate_date(3), 4)]
 
 
+# pylint: disable=redefined-outer-name
 @pytest.mark.order(2)
 @pytest.mark.parametrize(
     "file_version, expected_result",
     [
-        ("1", [("banana",  calculate_date(3), 4)]),
+        ("1", [("banana", calculate_date(3), 4)]),
         ("2", [(1, "meat", "ham", datetime.date(2024, 9, 30), 2)]),
-        ("3", [(1, "meat", "ham", datetime.date(2024, 9, 30), 2),
-               (2, "fruit", "banana",  calculate_date(3), 4)])
-    ]
+        (
+            "3",
+            [
+                (1, "meat", "ham", datetime.date(2024, 9, 30), 2),
+                (2, "fruit", "banana", calculate_date(3), 4),
+            ],
+        ),
+    ],
 )
-def test_display_products(product_repository, file_version, expected_result):
+def test_display_products(file_version, expected_result):
     # given
-    namespace = Namespace(json_file_display=f"tests/test_files/products_to_display_{file_version}.json")
+    namespace = Namespace(
+        json_file_display=f"tests/test_files/products_to_display_{file_version}.json"
+    )
 
     # when
     feedback = display_products(namespace)
@@ -59,6 +78,7 @@ def test_display_products(product_repository, file_version, expected_result):
     assert feedback == expected_result
 
 
+# pylint: disable=redefined-outer-name
 @pytest.mark.order(3)
 def test_remove_products(product_repository):
     # given
@@ -70,10 +90,12 @@ def test_remove_products(product_repository):
 
     # then
     assert product_repository.count() == 2
-    assert (product_repository.search(conditions={"name": "ham", "expiry_date": datetime.date(2024, 9, 30)})
-            == [(1, "meat", "ham", datetime.date(2024, 9, 30), 1)])
-    assert (product_repository.search(conditions={"name": "banana",  "expiry_date": calculate_date(3)})
-            == [(2, "fruit", "banana",  calculate_date(3), 2)])
+    assert product_repository.search(
+        conditions={"name": "ham", "expiry_date": datetime.date(2024, 9, 30)}
+    ) == [(1, "meat", "ham", datetime.date(2024, 9, 30), 1)]
+    assert product_repository.search(
+        conditions={"name": "banana", "expiry_date": calculate_date(3)}
+    ) == [(2, "fruit", "banana", calculate_date(3), 2)]
 
     # when
     remove_products(namespace)
